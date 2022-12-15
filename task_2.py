dc2f95a35b4e303a4c59fcba55f32300e2f39c23
Сучков Алексей <AlexiySV@yandex.ru>
2022-12-15 13:30:01 +0300

    task_2correct

--- task_2.py
+++ task_2.py
@@ -1,5 +1,6 @@
-#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-for x in [True,False]:
-    for y in [True,False]:
-        for z in [True,False]:
-            print(x,'AND',y,'OR',z,'=',x and y or z)
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            print('not(', x, 'or', y, 'or', z, ')=', 'not',
+                  x, 'and not', y, 'and not', z, '=', not (x or y or z) == (not x and not y and not z))
